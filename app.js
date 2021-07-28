6c62a354302da016c6de78c5fd9d893691a937a1
meteatbas <meteatbas154@gmail.com>
2021-07-29 00:07:26 +0300

    deploy

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-// const path = require('path')
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
+const cors=require('cors')
 const Vehicle=require('./models/Vehicle')
 const passport = require('passport')
 const vehicleRouter=require('./routes/index')
 const connectDB=require('./config/db')
 
 dotenv.config({ path: './config/config.env' })
+
+
 
 
 
@@ -51,9 +54,34 @@ app.use(function (req, res, next) {
 
 app.use(bodyParser.json())
 
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://shrouded-journey-38552.heroku']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/',vehicleRouter)
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/mercedes-api-front/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/mercedes-api-front/build', 'index.html'));
+  });
+}
 // app.use('/auth', require('./routes/auth'))
 // app.use('/stories', require('./routes/stories'))
 
